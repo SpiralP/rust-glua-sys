b31517202f695bf6d94d970af1bfd5b0a77496c2
SpiralP <spiral.palmer@gmail.com>
2020-03-22 10:35:44 -0700

    also add luaopen_ functions, some won't exist for lua_shared tho

--- build.rs
+++ build.rs
@@ -9,6 +9,7 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
     .header("LuaJIT/src/luajit.h")
+    .header("LuaJIT/src/lualib.h")
     // Tell cargo to invalidate the built crate whenever any of the
     // included header files changed.
     .parse_callbacks(Box::new(bindgen::CargoCallbacks))
