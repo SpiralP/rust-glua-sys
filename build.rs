51229028352c17b992da99d4de77dcfd4847af3a
SpiralP <spiral.palmer@gmail.com>
2020-04-11 20:03:00 -0700

    search multiple paths to find lua_shared

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
-const LUA_SHARED_DLL_PATH: &str =
-    r#"F:\SteamLibrary\steamapps\common\GarrysMod\bin\win64\lua_shared.dll"#;
-
-use std::{env, fs::File, io::Write, path::Path, process::Command};
+use std::{
+    env,
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -32,9 +35,84 @@ fn main() {
 
     #[cfg(not(feature = "ci"))]
     {
-        let lua_shared_lib_path = out_dir.join("lua_shared.lib");
-        let lua_shared_def_path = out_dir.join("lua_shared.def");
-        let lua_shared_dll_path = Path::new(LUA_SHARED_DLL_PATH);
+        link();
+    }
+}
+
+fn link() {
+    #[cfg(target_os = "windows")]
+    {
+        let out_dir = env::var("OUT_DIR").unwrap();
+        let out_dir = Path::new(&out_dir);
+
+        fn get_tool(name: &str) -> Command {
+            cc::windows_registry::find(&env::var("TARGET").unwrap(), name).unwrap()
+        }
+
+        fn get_exports<P: AsRef<Path>>(dll_path: P) -> Vec<String> {
+            let dll_path = dll_path.as_ref();
+
+            let output = get_tool("dumpbin.exe")
+                .arg("/EXPORTS")
+                .arg(dll_path)
+                .output()
+                .unwrap();
+
+            let stdout = String::from_utf8(output.stdout).unwrap();
+
+            stdout
+                .split("\r\n")
+                .map(|line| line.trim())
+                .skip_while(|line| line != &"ordinal hint RVA      name")
+                .skip(2)
+                .take_while(|line| line != &"")
+                .map(|line| line.split_whitespace().nth(3).unwrap())
+                .map(|line| line.to_string())
+                .collect::<Vec<_>>()
+        }
+
+        fn find_lua_shared_dll() -> Option<PathBuf> {
+            if let Ok(lua_shared_dll_path) = env::var("LUA_SHARED_DLL_PATH") {
+                return Some(PathBuf::from(lua_shared_dll_path));
+            }
+
+            let steam_paths: Vec<PathBuf> = [
+                r#"C:\Program Files (x86)\Steam"#,
+                r#"C:\SteamLibrary"#,
+                r#"D:\SteamLibrary"#,
+                r#"E:\SteamLibrary"#,
+                r#"F:\SteamLibrary"#,
+            ]
+            .iter()
+            .map(|a| a.into())
+            .collect();
+
+            for steam_path in steam_paths {
+                let lua_shared_dll_path = steam_path
+                    .join("steamapps")
+                    .join("common")
+                    .join("GarrysMod")
+                    .join("bin")
+                    .join("win64")
+                    .join("lua_shared.dll");
+
+                if lua_shared_dll_path.is_file() {
+                    return Some(lua_shared_dll_path);
+                }
+            }
+
+            None
+        }
+
+        let lua_shared_dll_path = find_lua_shared_dll().expect(
+            "lua_shared.dll couldn't be found! Try setting the environment variable \
+             LUA_SHARED_DLL_PATH to where lua_shared.dll is located",
+        );
+        assert_eq!(lua_shared_dll_path.extension().unwrap(), "dll");
+        let lua_shared_file_stem = lua_shared_dll_path.file_stem().unwrap().to_str().unwrap();
+
+        let lua_shared_lib_path = out_dir.join(format!("{}.lib", lua_shared_file_stem));
+        let lua_shared_def_path = out_dir.join(format!("{}.def", lua_shared_file_stem));
 
         {
             let exports = get_exports(&lua_shared_dll_path);
@@ -57,32 +135,6 @@ fn main() {
             .success());
 
         println!("cargo:rustc-link-search=native={}", out_dir.display());
-        println!("cargo:rustc-link-lib=dylib=lua_shared");
+        println!("cargo:rustc-link-lib=dylib={}", lua_shared_file_stem);
     }
-}
-
-fn get_tool(name: &str) -> Command {
-    cc::windows_registry::find(&env::var("TARGET").unwrap(), name).unwrap()
-}
-
-fn get_exports<P: AsRef<Path>>(dll_path: P) -> Vec<String> {
-    let dll_path = dll_path.as_ref();
-
-    let output = get_tool("dumpbin.exe")
-        .arg("/EXPORTS")
-        .arg(dll_path)
-        .output()
-        .unwrap();
-
-    let stdout = String::from_utf8(output.stdout).unwrap();
-
-    stdout
-        .split("\r\n")
-        .map(|line| line.trim())
-        .skip_while(|line| line != &"ordinal hint RVA      name")
-        .skip(2)
-        .take_while(|line| line != &"")
-        .map(|line| line.split_whitespace().nth(3).unwrap())
-        .map(|line| line.to_string())
-        .collect::<Vec<_>>()
 }
